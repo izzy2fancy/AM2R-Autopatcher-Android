90c9745a9eb633f5a5a7a357882e68e10e3a8cc1
Izzy Kauffman <121840901+izzy2fancy@users.noreply.github.com>
2024-03-28 19:20:30 -0400

    Update patcher.sh

--- patcher.sh
+++ patcher.sh
@@ -41,8 +41,7 @@ fi
 
 # Download data folder
 
-wget https://github.com/izzy2fancy/AM2R-Autopatcher-Android/releases/download/1.0/data.zip 
-yes | unzip data.zip -d ./
+wget https://github.com/izzy2fancy/AM2R-Autopatcher-Android/releases/download/1.0/data.zip unzip data.zip -d ./
 
 # Check for AM2R_11.zip in downloads
 if [ -f ~/storage/downloads/AM2R_11.zip ]; then
